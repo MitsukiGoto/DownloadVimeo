3aa4a6928c426af513531fa25bf6fd767cc41183
mitsukigoto <mi22kknsinge@ezweb.ne.jp>
2021-02-05 05:10:18 +0900

    code formatting by VSCode

--- vimeo_download/main.cpp
+++ vimeo_download/main.cpp
@@ -16,57 +16,63 @@
 #include "include/model/parsedArg.hpp"
 #include "Requests.hpp"
 
-namespace {
-auto parseArgument(int argc, const char** argv) {
-    std::tuple<std::string, std::string, bool, bool> options;
-    boost::program_options::options_description description("Download Vimeo Video even though it is set Private \n"
-                                                            "Usage: vimeo_download [-u URL] [-o Output]");
-    description.add_options()
-    ("url,u", boost::program_options::value<std::string>(), "Set Target URL")
-    ("output,o", boost::program_options::value<std::string>(), "Set output name")
-    ("continue,c", "Other Download")
-    ("help,h", "Help")
-    ("verbose", "Turn on Verbose Mode")
-    ("version", "Show Version");
-    
-    boost::program_options::variables_map vm;
-    store(boost::program_options::parse_command_line(argc, argv, description), vm);
-    notify(vm);
-    
-    if(vm.count("help")) {
-        std::cout << description << std::endl;
-        std::exit(0);
-    }
-    if(vm.count("version")) {
-        std::cout << "Download Vimeo (v2.0)" << std::endl;
-        std::exit(0);
-    }
-    if(!vm.count("url") || !vm.count("output")) {
-        std::cout << "\e[36m" << "\n";
-        std::cerr << "Required URL or OUTPUT Option" << std::endl;
-        std::cout << "\e[m" << "\n";
-        std::exit(1);
-    }
-    std::get<0>(options) = vm["output"].as<std::string>();
-    std::get<1>(options) = vm["url"].as<std::string>();
-    if(vm.count("verbose")) {
-        std::get<2>(options) = true;
-    } else {
-        std::get<2>(options) = false;
-    }
-    auto parsedArg = std::make_unique<ParsedArg>(std::get<0>(options), std::get<1>(options), std::get<2>(options));
-    return parsedArg;
-}
-}
+namespace
+{
+    auto parseArgument(int argc, const char **argv)
+    {
+        std::tuple<std::string, std::string, bool, bool> options;
+        boost::program_options::options_description description("Download Vimeo Video even though it is set Private \n"
+                                                                "Usage: vimeo_download [-u URL] [-o Output]");
+        description.add_options()("url,u", boost::program_options::value<std::string>(), "Set Target URL")("output,o", boost::program_options::value<std::string>(), "Set output name")("continue,c", "Other Download")("help,h", "Help")("verbose", "Turn on Verbose Mode")("version", "Show Version");
 
-int main(int argc, const char * argv[]) {
+        boost::program_options::variables_map vm;
+        store(boost::program_options::parse_command_line(argc, argv, description), vm);
+        notify(vm);
+
+        if (vm.count("help"))
+        {
+            std::cout << description << std::endl;
+            std::exit(0);
+        }
+        if (vm.count("version"))
+        {
+            std::cout << "Download Vimeo (v2.0)" << std::endl;
+            std::exit(0);
+        }
+        if (!vm.count("url") || !vm.count("output"))
+        {
+            std::cout << "\e[36m"
+                      << "\n";
+            std::cerr << "Required URL or OUTPUT Option" << std::endl;
+            std::cout << "\e[m"
+                      << "\n";
+            std::exit(1);
+        }
+        std::get<0>(options) = vm["output"].as<std::string>();
+        std::get<1>(options) = vm["url"].as<std::string>();
+        if (vm.count("verbose"))
+        {
+            std::get<2>(options) = true;
+        }
+        else
+        {
+            std::get<2>(options) = false;
+        }
+        auto parsedArg = std::make_unique<ParsedArg>(std::get<0>(options), std::get<1>(options), std::get<2>(options));
+        return parsedArg;
+    }
+} // namespace
+
+int main(int argc, const char *argv[])
+{
     //     Expected: output, url
     auto arg = parseArgument(argc, argv);
-    if(arg->isVerbose) std::cout << "Verbose Mode on" << std::endl;
+    if (arg->isVerbose)
+        std::cout << "Verbose Mode on" << std::endl;
     auto vimeo = std::make_unique<Vimeo>(arg->outputName, arg->url, Requests::get(arg->url), arg->isVerbose);
     vimeo->download().merge();
 #ifdef __APPLE__
-        std::system("osascript -e 'display notification \"Finish\"'");
+    std::system("osascript -e 'display notification \"Finish\"'");
 #endif
     return 0;
 }
--- vimeo_download/src/Vimeo.cpp
+++ vimeo_download/src/Vimeo.cpp
@@ -20,17 +20,21 @@
 #include <sstream>
 #include <iterator>
 
-namespace {
-auto decode = [](std::string encodedStr){
-    return cppcodec::base64_rfc4648::decode(encodedStr);
-};
-std::string remove_str(std::string from, std::string regex) {
-    return std::regex_replace(from, std::regex(regex), "");;
-}
+namespace
+{
+    auto decode = [](std::string encodedStr) {
+        return cppcodec::base64_rfc4648::decode(encodedStr);
+    };
+    std::string remove_str(std::string from, std::string regex)
+    {
+        return std::regex_replace(from, std::regex(regex), "");
+        ;
+    }
 
-}
+} // namespace
 
-Vimeo::Vimeo(const std::string& output_name, const std::string& url, std::unique_ptr<JSON> json, bool isVerbose) : url(url) ,output_name(output_name), isVerbose(isVerbose) {
+Vimeo::Vimeo(const std::string &output_name, const std::string &url, std::unique_ptr<JSON> json, bool isVerbose) : url(url), output_name(output_name), isVerbose(isVerbose)
+{
     this->json = std::move(json);
     this->base_url = std::regex_replace(url, std::regex(R"(master.json?.+?base64_init=1)"), "");
     this->base_url = url;
@@ -40,45 +44,56 @@ Vimeo::Vimeo(const std::string& output_name, const std::string& url, std::unique
     this->tmp_dir = paths[0];
     this->save_dir = paths[1];
     std::cout << output_name << std::endl;
-    if(this->output_name.find(".mp4") == std::string::npos) {
-        if(this->isVerbose) {
+    if (this->output_name.find(".mp4") == std::string::npos)
+    {
+        if (this->isVerbose)
+        {
             std::cout << "cannot find .mp4 with name" << std::endl;
         }
-        this->output_name+=".mp4";
+        this->output_name += ".mp4";
     }
 }
 
-template<typename T, typename U>
-auto Vimeo::command(T tmp, U name) {
+template <typename T, typename U>
+auto Vimeo::command(T tmp, U name)
+{
     auto command = "ffmpeg -i " + tmp + "a.mp3 " + "-i " + tmp + "v.mp4" + " -acodec" + " copy" + " -vcodec" + " copy " + this->home_dir + "/Desktop/Vimeo/" + name;
     return command;
 }
 
-void Vimeo::merge() {
+void Vimeo::merge()
+{
 #if defined(__MACH__) || defined(__linux)
     auto command_ = command(this->tmp_dir, this->output_name);
-    if(!this->isVerbose) {
+    if (!this->isVerbose)
+    {
         command_ += " >/dev/null 2>&1";
-    } else {
+    }
+    else
+    {
         std::cout << command_ << std::endl;
     }
     std::system(command_.c_str());
-    std::cout << "\u001b[35m" << "Merging Audio and Video has successflly done" << std::endl;
+    std::cout << "\u001b[35m"
+              << "Merging Audio and Video has successflly done" << std::endl;
     std::cout << "The Video was saved to:" << this->home_dir << "/Desktop/Vimeo/" << this->output_name << std::endl;
     std::filesystem::remove_all(this->tmp_dir);
     std::cout << "Cleaning Process Started" << std::endl;
-    std::cout << "\u001b[31m" << "Temp Folder has removed: " << this->tmp_dir << std::endl;
-    std::cout << "Cleaning Process End" << "\u001b[0m" << std::endl;
+    std::cout << "\u001b[31m"
+              << "Temp Folder has removed: " << this->tmp_dir << std::endl;
+    std::cout << "Cleaning Process End"
+              << "\u001b[0m" << std::endl;
 #endif
 #ifdef __WIN64
     std::cout << "Sorry, Merging Audio and Video doesn't support on Windows now" << std::endl;
 #endif
 }
 
-Vimeo& Vimeo::download() {
+Vimeo &Vimeo::download()
+{
 #ifdef __MACH__
-    auto process1 = std::thread([this]{this->downloadVideo();});
-    auto process2 = std::thread([this]{this->downloadAudio();});
+    auto process1 = std::thread([this] { this->downloadVideo(); });
+    auto process2 = std::thread([this] { this->downloadAudio(); });
     process1.join();
     process2.join();
 #else
@@ -88,26 +103,29 @@ Vimeo& Vimeo::download() {
     return *this;
 }
 
-std::array<std::string, 2> Vimeo::createDirectory() {
+std::array<std::string, 2> Vimeo::createDirectory()
+{
     std::array<std::string, 2> paths;
     auto now_c = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     std::stringstream ss;
     ss << now_c;
-    std::string tmp = std::filesystem::temp_directory_path().string()+"/dlvimeo/"+ss.str()+"/";
+    std::string tmp = std::filesystem::temp_directory_path().string() + "/dlvimeo/" + ss.str() + "/";
     std::filesystem::create_directories(tmp);
     paths[0] = tmp;
-    std::string saved = this->home_dir+"/Desktop/Vimeo";
+    std::string saved = this->home_dir + "/Desktop/Vimeo";
     std::filesystem::create_directories(saved);
     paths[1] = saved;
     return paths;
 }
 
-void Vimeo::downloadVideo() {
+void Vimeo::downloadVideo()
+{
     auto video = json->v.get<picojson::object>()["video"].get<picojson::array>();
     int i = 0;
     // The most high quality video has most largest "height" value
     std::vector<std::tuple<int, int>> heights;
-    for(auto& content:video) {
+    for (auto &content : video)
+    {
         std::string height = content.get<picojson::object>()["height"].to_str();
         std::tuple<int, int> t = {std::stoi(height), i};
         heights.push_back(t);
@@ -116,44 +134,55 @@ void Vimeo::downloadVideo() {
     std::sort(std::begin(heights), std::end(heights), std::greater<std::tuple<int, int>>());
     picojson::object highest_video = video[std::get<1>(heights[0])].get<picojson::object>();
     std::string video_base_url = remove_str(url, "master.json?.+?base64_init=1") + highest_video.at("base_url").to_str();
-    if(this->isVerbose) {
-        std::cout << "video_base_url: " << video_base_url <<  std::endl;
+    if (this->isVerbose)
+    {
+        std::cout << "video_base_url: " << video_base_url << std::endl;
     }
     auto decoded = decode(highest_video.at("init_segment").to_str());
-    std::ofstream ofs(this->tmp_dir+"v.mp4", std::ios::out|std::ios::binary);
+    std::ofstream ofs(this->tmp_dir + "v.mp4", std::ios::out | std::ios::binary);
     std::stringstream s;
     std::move(decoded.begin(), decoded.end(), std::ostream_iterator<unsigned char>(s));
     ofs << s.rdbuf();
     ofs.close();
-    for(auto& segment:highest_video.at("segments").get<picojson::array>()) {
-        std::string segment_url = video_base_url+segment.get<picojson::object>()["url"].to_str();
-        Requests::get(segment_url, this->tmp_dir+"v.mp4");
+    for (auto &segment : highest_video.at("segments").get<picojson::array>())
+    {
+        std::string segment_url = video_base_url + segment.get<picojson::object>()["url"].to_str();
+        Requests::get(segment_url, this->tmp_dir + "v.mp4");
     }
-    std::cout << "\u001b[36m" << "Downloading Video has successflly done" << "\u001b[0m" << std::endl;
-    if(this->isVerbose){
-        std::cout << "Only Video at: " << this->tmp_dir+"v.mp4" <<  std::endl;
+    std::cout << "\u001b[36m"
+              << "Downloading Video has successflly done"
+              << "\u001b[0m" << std::endl;
+    if (this->isVerbose)
+    {
+        std::cout << "Only Video at: " << this->tmp_dir + "v.mp4" << std::endl;
     }
 }
 
-void Vimeo::downloadAudio() {
+void Vimeo::downloadAudio()
+{
     auto audio = json->v.get<picojson::object>()["audio"].get<picojson::array>()[0].get<picojson::object>();
     std::string audio_base_url = remove_str(remove_str(url, "master.json?.+?base64_init=1"), "sep/.+") + "sep/audio/" + remove_str(audio.at("base_url").to_str(), "../../audio");
-    if(this->isVerbose){
+    if (this->isVerbose)
+    {
         std::cout << audio.at("base_url") << std::endl;
-        std::cout << "audio_base_url: " << audio_base_url <<  std::endl;
+        std::cout << "audio_base_url: " << audio_base_url << std::endl;
     }
     auto decoded = decode(audio.at("init_segment").to_str());
-    std::ofstream ofs(this->tmp_dir+"a.mp3", std::ios::out|std::ios::binary);
+    std::ofstream ofs(this->tmp_dir + "a.mp3", std::ios::out | std::ios::binary);
     std::stringstream s;
     std::move(decoded.begin(), decoded.end(), std::ostream_iterator<unsigned char>(s));
     ofs << s.rdbuf();
     ofs.close();
-    for(auto& segment:audio.at("segments").get<picojson::array>()) {
-        std::string segment_url = audio_base_url+segment.get<picojson::object>()["url"].to_str();
-        Requests::get(segment_url, this->tmp_dir+"a.mp3");
+    for (auto &segment : audio.at("segments").get<picojson::array>())
+    {
+        std::string segment_url = audio_base_url + segment.get<picojson::object>()["url"].to_str();
+        Requests::get(segment_url, this->tmp_dir + "a.mp3");
     }
-    std::cout << "\u001b[32m" << "Downloading Audio has successflly done" << "\u001b[0m" << std::endl;
-    if(this->isVerbose){
-        std::cout << "Only Audio at: " << this->tmp_dir+"v.mp4" <<  std::endl;
+    std::cout << "\u001b[32m"
+              << "Downloading Audio has successflly done"
+              << "\u001b[0m" << std::endl;
+    if (this->isVerbose)
+    {
+        std::cout << "Only Audio at: " << this->tmp_dir + "v.mp4" << std::endl;
     }
 }
